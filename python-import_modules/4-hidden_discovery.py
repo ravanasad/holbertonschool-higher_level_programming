200e3fbbff3224a39c3b07af37e23f7952260f4c
Ravan Asad <ravanasad@outlook.com>
2024-01-27 03:47:45 -0800

    Updated

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import hidden_4
 
 if __name__ == "__main__":
-    names = dir("hidden_4")
-    for x in sorted(names):
+    names = dir(hidden_4)
+    for x in names:
         if not x.startswith('__'):
             print(x)
