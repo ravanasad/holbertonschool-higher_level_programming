72180951637f889a2b636ac385d57ea3fe49ef43
Ravan Asad <ravanasad@outlook.com>
2024-01-27 03:20:16 -0800

    git ignore

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ from sys import argv
 
 if __name__ == "__main__":
     result = 0
-    for x in range(1 , len(argv)):
+    for x in range(1, len(argv)):
         result += int(argv[x])
     print(result)
