6e25546fa2cc7881bafbc6757070ff6d49f38775
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-02-02 18:42:10 +0400

    style fix

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -5,4 +5,3 @@ def only_diff_elements(set_1, set_2):
     print(c_set)
     set_1.update(set_2)
     return {x for x in set_1 if x not in c_set}
-
--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -2,7 +2,3 @@
 
 def number_keys(a_dictionary):
     return len(a_dictionary)
-
-a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-nb_keys = number_keys(a_dictionary)
-print("Number of keys: {:d}".format(nb_keys))
