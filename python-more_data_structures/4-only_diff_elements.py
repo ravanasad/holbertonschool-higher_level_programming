09be01c30884c56db11017c0df0e74c6740c5dc7
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-02-02 18:35:28 +0400

    All tasks Created

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    c_set = {x for x in set_1 if x in set_2}
+    print(c_set)
+    set_1.update(set_2)
+    return {x for x in set_1 if x not in c_set}
+
