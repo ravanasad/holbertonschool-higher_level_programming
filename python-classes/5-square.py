e4c15400aa5ee4831d54f06a13402fe0f9f67eb5
Ravan Asadov <ravanasadovv@gmail.com>
2024-02-03 14:19:54 -0800

    Style

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         return self.__size * self.__size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -18,6 +21,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def my_print(self):
         if self.__size == 0:
             print()
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         return self.__size * self.__size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -18,14 +21,17 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def my_print(self):
         if self.__size == 0:
             print()
         for i in range(self.__size):
             print("#" * self.__size)
+
     @property
     def position(self):
         return self.__position
+
     @position.setter
     def position(self, value):
         if value[0] < 0 or value[1] < 0:
