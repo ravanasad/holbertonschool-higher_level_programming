33755f64ee75328f77ebdefaddc97e74569b147f
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-02-04 01:51:35 +0400

    Tasks Created

---
+++ python-classes/6-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size = 0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.__size * self.__size
+    def size(self):
+        return self.__size
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
+    def position(self):
+        return self.__position
+    def position(self, value):
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,10 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except (ValueError, TypeError):
         return False
+    
+value = {89}
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
