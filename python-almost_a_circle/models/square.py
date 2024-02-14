9d6fecc5a7df1ed4202d39c95ab5f5433763a6aa
Ravan Asad <ravanasad@gmail.com>
2024-02-14 10:46:52 -0800

    Task 11 Created

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,3 +27,28 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        i = 0
+        if args is not None and len(args) > 0:
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+        else:
+            for key in kwargs:
+                if key == "id":
+                    self.id = kwargs[key]
+                elif key == "size":
+                    self.size = kwargs[key]
+                elif key == "x":
+                    self.x = kwargs[key]
+                elif key == "y":
+                    self.y = kwargs[key]
