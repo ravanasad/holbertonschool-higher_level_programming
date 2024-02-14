cc49af69a74f168ab6a5c24de8d3524cf270ea83
Ravan Asad <ravanasad@gmail.com>
2024-02-14 10:59:54 -0800

    Task 15 Created

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ This module defines a class for a Base
 """
 
 
+import json
+
+
 class Base:
     """Defines a base object.
     Attributes:
@@ -18,3 +21,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(ld):
+        """returns the JSON string representation of list_dictionaries"""
+        if ld is None or len(ld) == 0:
+            return []
+        return json.dumps(ld)
