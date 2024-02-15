d9c4b5a95ca3e30628e47b3a34816369d2aad684
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-02-15 16:46:58 +0400

    Base Test created

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+    This module defines a Test class for a Base
+"""
+
+
+import unittest
+
+
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    
+    def test_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+    
+    def test_id_2(self):
+        b2 = Base(89)
+        self.assertEqual(b2.id, 89)
+
+    def test_id_3(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
+    
+    def test_to_json_string_without_arg(self):
+        json_dictionary = Base.to_json_string([])
+        self.assertEqual(json_dictionary, "[]")
+
+    def test_to_json_string_with_arg(self):
+        json_dictionary = Base.to_json_string([{'id': 89}])
+        self.assertEqual(json_dictionary, '[{"id": 89}]')
+
+    def test_to_json_string_with_none(self):
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, "[]")
+    
+    def test_from_json_string_empty(self):
+        json_dictionary = Base.from_json_string("[]")
+        self.assertEqual(json_dictionary, [])
+
+    def test_from_json_string(self):
+        json_dictionary = Base.from_json_string('[{"id": 89}]')
+        self.assertEqual(json_dictionary, [{'id': 89}])
+
+if __name__ == "__main__":
+    unittest.main()
+    
+
+    
+
+
---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    This module defines a Test class for a Rectangle
+"""
+
+
+import unittest
+
+
+from models.base import Base
+from models.rectangle import Rectangle
