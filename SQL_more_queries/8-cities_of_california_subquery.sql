b57ece14c3b542e23c001230d9c11483ce47ea0d
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-03-20 16:24:58 +0400

    Update 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 
-SELECT C.id, C.name FROM cities C
-JOIN states S
-ON S.id = C.state_id
+SELECT C.id, C.name FROM cities AS C
+WHERE C.state_id = (SELECT S.id FROM states AS S WHERE S.name = 'California');
