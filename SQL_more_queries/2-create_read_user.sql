6aa17ede36fe5d2c685d3e1001b851585bac2b0b
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-03-20 16:21:19 +0400

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,13 +1,12 @@
 -- creates the database hbtn_0d_2
 -- creates the user user_0d_2
 
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+CREATE DATABASE 
+IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER 
+IF NOT EXISTS 'user_0d_2'@'localhost' 
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost';
