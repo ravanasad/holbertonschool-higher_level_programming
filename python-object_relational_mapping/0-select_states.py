eba73ce6b4e2c4cfa8504c543740e38a56b89466
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-03-23 20:49:04 +0400

    Created 0-select_states.py
    Added all files and Created  0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""0-select_states.py: lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+
+
+def get_states():
+    """get_states: lists all states from the database hbtn_0e_0_usa
+
+        Args:
+            username, password, database_name -> argv[1], argv[2], argv[3]
+    """
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+        
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    get_states()
