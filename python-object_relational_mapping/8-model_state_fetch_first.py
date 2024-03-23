8134e5c021cabf4788ab5678afda51d0efee57e6
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-03-23 21:57:53 +0400

    Update 8-model_state_fetch_first.py

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def fetch_all():
+    """prints the first State object from the database hbtn_0e_6_usa"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    row = session.query(State).first()
+    print("{}: {}".format(row.id, row.name))
+    session.close()
+
+
+if __name__ == "__main__":
+    fetch_all()
